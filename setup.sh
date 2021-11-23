15742ba762ef9ec2a4c7f4883d8a52a96b2fedba
Patrick Wieth <patrick.wieth@googlemail.com>
2021-11-23 15:48:38 +0100

    fix bugs

--- setup.sh
+++ setup.sh
@@ -10,13 +10,13 @@ VERSION="v1.0.0"
 
 
 #create cookbook
-#pylonsd tx pylons create-cookbook $GAME_NAME $GAME_NAME "A place for NFTs to fight" $DEV_Name $VERSION $DEV_MAIL "{\"denom\":\"$TOKEN_NAME\",\"amount\":\"50\"}" true --from $ACC_NAME
+pylonsd tx pylons create-cookbook $GAME_NAME $GAME_NAME "A place for NFTs to fight" $DEV_NAME $VERSION $DEV_MAIL "{\"denom\":\"$TOKEN_NAME\",\"amount\":\"50\"}" true --from $DEV_NAME
 
 #create faucet
-#pylonsd tx pylons create-recipe $GAME_NAME getcoin getcoin "Get some coins to test" $VERSION '[]' '[]' '{"coinOutputs": [{"ID":"coinShower",  "coin":{"denom":'$TOKEN_NAME',"amount": "100000"}}], "itemOutputs": [],"itemModifyOutputs":[]}' '[{"entryIDs": ["coinShower"],"weight": 1}]' 1 true "" --from $ACC_NAME
+pylonsd tx pylons create-recipe $GAME_NAME getcoin getcoin "Get some coins to test" $VERSION '[]' '[]' '{"coinOutputs": [{"ID":"coinShower",  "coin":{"denom":'$TOKEN_NAME',"amount": "100000"}}], "itemOutputs": [],"itemModifyOutputs":[]}' '[{"entryIDs": ["coinShower"],"weight": 1}]' 1 true "" --from $DEV_NAME
 
 #create mintweapon
-#sh ./recipes/mintWeaponRecipe.sh $GAME_NAME $VERSION $DEV_NAME $TOKEN_NAME_EXT
+sh ./recipes/mintWeaponRecipe.sh $GAME_NAME $VERSION $DEV_NAME $TOKEN_NAME_EXT
 
 #create enchantWeapon
 sh ./recipes/enchantWeaponRecipe.sh $GAME_NAME $VERSION $DEV_NAME $TOKEN_NAME_EXT
