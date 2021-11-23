31047a9f5471cc7116dfd5ee73cd2e21ebd83b35
nlsui <marius@itc-faust.de>
2021-11-23 18:16:48 +0100

    Fixed enchanting

--- recipes/enchantWeaponRecipe.sh
+++ recipes/enchantWeaponRecipe.sh
@@ -5,4 +5,8 @@ VERSION=$2
 ACC_NAME=$3
 TOKEN_NAME_EXT=$4
 
-pylonsd tx pylons create-recipe $GAME_NAME enchantweapon enchantweapon "enchant a weapon with a random enchantment" $VERSION '[{"coins": [{"denom": "'$TOKEN_NAME_EXT'", "amount": "100"}]}]' '[{"ID":"weapon","doubles":[],"longs":[],"strings":[{"key":"ItemType","value":"weapon"}],"conditions":{"doubles":[],"longs":[],"strings":[]}}]' '{"coinOutputs": [],"itemOutputs": [],"itemModifyOutputs":[{"ID":"enchantedWeapon","itemInputRef":"weapon","doubles":[],"longs":[],"strings":[{"key":"Enchantment","value":"blingbling"}],"mutableStrings":[],"transferFee":[],"tradePercentage":"0.100000000000000000","tradeable":true}]}' '[{"entryIDs": ["enchantedWeapon"],"weight": 1}]' 1 true "" --from $ACC_NAME
+#sword is used as a dummy-parameter tor prevent IndexOutOfBoundsError in itemOutputs
+SWORD_ID="sword"
+SWORD=`cat ./items/$SWORD_ID.json`
+
+pylonsd tx pylons create-recipe $GAME_NAME enchantweapon enchantweapon "enchant a weapon with a random enchantment" $VERSION '[{"coins": [{"denom": "'$TOKEN_NAME_EXT'", "amount": "100"}]}]' '[{"ID":"weapon","doubles":[],"longs":[],"strings":[],"conditions":{"doubles":[],"longs":[],"strings":[{"key":"ItemType","value":"weapon"},{"key":"Enchantment","value":"none"}]}}]' '{"coinOutputs": [],"itemOutputs": ['$SWORD'],"itemModifyOutputs":[{"ID":"enchantedWeapon","itemInputRef":"weapon","doubles":[],"longs":[],"strings":[{"key":"Enchantment","value":"blingbling"}],"mutableStrings":[],"transferFee":[],"tradePercentage":"0.100000000000000000","tradeable":true}]}' '[{"entryIDs": ["enchantedWeapon"],"weight": 1}]' 1 true "" --from $ACC_NAME
--- setup.sh
+++ setup.sh
@@ -8,15 +8,25 @@ TOKEN_NAME_EXT=$GAME_NAME"/"$TOKEN_NAME
 VERSION="v1.0.0"
 
 
+#create key
+pylonsd keys add $DEV_NAME
+
+#read key
+KEY=$(pylonsd keys show $DEV_NAME --address)
+
+#create account
+pylonsd tx pylons create-account $DEV_NAME --from $KEY
 
 #create cookbook
 pylonsd tx pylons create-cookbook $GAME_NAME $GAME_NAME "A place for NFTs to fight" $DEV_NAME $VERSION $DEV_MAIL "{\"denom\":\"$TOKEN_NAME\",\"amount\":\"50\"}" true --from $DEV_NAME
 
 #create faucet
-pylonsd tx pylons create-recipe $GAME_NAME getcoin getcoin "Get some coins to test" $VERSION '[]' '[]' '{"coinOutputs": [{"ID":"coinShower",  "coin":{"denom":'$TOKEN_NAME',"amount": "100000"}}], "itemOutputs": [],"itemModifyOutputs":[]}' '[{"entryIDs": ["coinShower"],"weight": 1}]' 1 true "" --from $DEV_NAME
+pylonsd tx pylons create-recipe $GAME_NAME getcoins getcoins "Get some coins to test" $VERSION '[]' '[]' '{"coinOutputs": [{"ID":"coinShower",  "coin":{"denom":"'$TOKEN_NAME'","amount": "100000"}}], "itemOutputs": [],"itemModifyOutputs":[]}' '[{"entryIDs": ["coinShower"],"weight": 1}]' 1 true "" --from $DEV_NAME
 
 #create mintweapon
 sh ./recipes/mintWeaponRecipe.sh $GAME_NAME $VERSION $DEV_NAME $TOKEN_NAME_EXT
 
 #create enchantWeapon
 sh ./recipes/enchantWeaponRecipe.sh $GAME_NAME $VERSION $DEV_NAME $TOKEN_NAME_EXT
+
+echo $KEY
