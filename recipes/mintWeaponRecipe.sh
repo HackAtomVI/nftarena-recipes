fd5743000c5ca0e495aec4bbf6aadff80ed0bd17
nlsui <marius@itc-faust.de>
2021-11-24 13:16:55 +0100

    Added more weapons

--- recipes/mintWeaponRecipe.sh
+++ recipes/mintWeaponRecipe.sh
@@ -13,4 +13,4 @@ SWORD=`cat ./items/$SWORD_ID.json`
 GREATSWORD=`cat ./items/$GREATSWORD_ID.json`
 AXE=`cat ./items/$AXE_ID.json`
 
-pylonsd tx pylons create-recipe $GAME_NAME mintweapon mintweapon "mint a random weapon" $VERSION '[{"coins": [{"denom": "'$TOKEN_NAME_EXT'", "amount": "100"}]}]' '[]' '{"coinOutputs": [],"itemOutputs": ['$SWORD','$GREATSWORD','$AXE'],"itemModifyOutputs":[]}' '[{"entryIDs": ["'$SWORD_ID'"],"weight": 1},{"entryIDs": ["'$GREATSWORD_ID'"],"weight": 1},{"entryIDs": ["'$AXE_ID'"],"weight": 1}]' 1 true "" --from $ACC_NAME
+pylonsd tx pylons create-recipe $GAME_NAME mintweapon mintweapon "mint a random weapon" $VERSION '[{"coins": [{"denom": "'$TOKEN_NAME_EXT'", "amount": "100"}]}]' '[]' '{"coinOutputs": [],"itemOutputs": ['$SWORD','$GREATSWORD','$AXE'],"itemModifyOutputs":[]}' '[{"entryIDs": ["'$AXE_ID'"],"weight": 1},{"entryIDs": ["'$SWORD_ID'"],"weight": 1},{"entryIDs": ["'$GREATSWORD_ID'"],"weight": 1}]' 1 true "" --from $ACC_NAME
