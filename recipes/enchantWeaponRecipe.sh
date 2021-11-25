b2c0f5bc22fd23f9d820f257b581def95866860e
nlsui <marius@itc-faust.de>
2021-11-25 14:36:00 +0100

    Updated enchantWeaponRecipe

--- recipes/enchantWeaponRecipe.sh
+++ recipes/enchantWeaponRecipe.sh
@@ -5,14 +5,18 @@ VERSION=$2
 ACC_NAME=$3
 TOKEN_NAME_EXT=$4
 
-WIELDY="wieldy"
-SHARP="sharp"
-LIGHT="light"
-OVERSIZED="oversized"
-HEALTHY="healthy"
+WIELDY_ID="wieldy"
+SHARP_ID="sharp"
+LIGHT_ID="light"
+OVERSIZED_ID="oversized"
+
+WIELDY=`cat ./enchantments/$WIELDY_ID.json`
+SHARP=`cat ./enchantments/$SHARP_ID.json`
+LIGHT=`cat ./enchantments/$LIGHT_ID.json`
+OVERSIZED=`cat ./enchantments/$OVERSIZED_ID.json`
 
 #sword is used as a dummy-parameter tor prevent IndexOutOfBoundsError in itemOutputs
 SWORD_ID="sword"
 SWORD=`cat ./items/$SWORD_ID.json`
 
-pylonsd tx pylons create-recipe $GAME_NAME enchantweapon enchantweapon "enchant a weapon with a random enchantment" $VERSION '[{"coins": [{"denom": "'$TOKEN_NAME_EXT'", "amount": "100"}]}]' '[{"ID":"weapon","doubles":[],"longs":[],"strings":[],"conditions":{"doubles":[],"longs":[],"strings":[{"key":"ItemType","value":"weapon"},{"key":"Enchantment","value":"none"}]}}]' '{"coinOutputs": [],"itemOutputs": ['$SWORD'],"itemModifyOutputs":[{"ID":"'$WIELDY'","itemInputRef":"weapon","doubles":[],"longs":[],"strings":[{"key":"Enchantment","value":"'$WIELDY'"}],"mutableStrings":[],"transferFee":[],"tradePercentage":"0.100000000000000000","tradeable":true},{"ID":"'$SHARP'","itemInputRef":"weapon","doubles":[],"longs":[],"strings":[{"key":"Enchantment","value":"'$SHARP'"}],"mutableStrings":[],"transferFee":[],"tradePercentage":"0.100000000000000000","tradeable":true},{"ID":"'$LIGHT'","itemInputRef":"weapon","doubles":[],"longs":[],"strings":[{"key":"Enchantment","value":"'$LIGHT'"}],"mutableStrings":[],"transferFee":[],"tradePercentage":"0.100000000000000000","tradeable":true},{"ID":"'$OVERSIZED'","itemInputRef":"weapon","doubles":[],"longs":[],"strings":[{"key":"Enchantment","value":"'$OVERSIZED'"}],"mutableStrings":[],"transferFee":[],"tradePercentage":"0.100000000000000000","tradeable":true},{"ID":"'$HEALTHY'","itemInputRef":"weapon","doubles":[],"longs":[],"strings":[{"key":"Enchantment","value":"'$HEALTHY'"}],"mutableStrings":[],"transferFee":[],"tradePercentage":"0.100000000000000000","tradeable":true}]}' '[{"entryIDs": ["'$WIELDY'"],"weight": 1},{"entryIDs": ["'$SHARP'"],"weight": 1},{"entryIDs": ["'$LIGHT'"],"weight": 1},{"entryIDs": ["'$OVERSIZED'"],"weight": 1},{"entryIDs": ["'$HEALTHY'"],"weight": 1}]' 1 true "" --from $ACC_NAME
+pylonsd tx pylons create-recipe $GAME_NAME enchantweapon enchantweapon "enchant a weapon with a random enchantment" $VERSION '[{"coins": [{"denom": "'$TOKEN_NAME_EXT'", "amount": "100"}]}]' '[{"ID":"weapon","doubles":[{"key":"damage","minValue":"0","maxValue":"100"},{"key":"accuracy","minValue":"0","maxValue":"10"}],"longs":[],"strings":[],"conditions":{"doubles":[],"longs":[],"strings":[{"key":"ItemType","value":"weapon"},{"key":"Enchantment","value":"none"}]}}]' '{"coinOutputs": [],"itemOutputs": ['$SWORD'],"itemModifyOutputs":['$WIELDY','$SHARP','$LIGHT','$OVERSIZED']}' '[{"entryIDs": ["'$WIELDY_ID'"],"weight": 1},{"entryIDs": ["'$SHARP_ID'"],"weight": 1},{"entryIDs": ["'$LIGHT_ID'"],"weight": 1},{"entryIDs": ["'$OVERSIZED_ID'"],"weight": 1}]' 1 true "" --from $ACC_NAME
